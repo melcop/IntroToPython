06d76df278ba18bd9a2c16be8e5a9979c71d0111
MelCop <charliedem20@gmail.com>
2023-04-02 11:31:07 +0200

    uptodate

--- KEA_python/student.py
+++ KEA_python/student.py
@@ -6,9 +6,22 @@ class Student:
         self.address = address
         self.age = age
 
+    def return_name(self):
+        """return student name"""
+        return self.name
+    
+    def return_age(self):
+        """return student age"""
+        return self.age
+    
+    def return_address(self):
+        """return student address"""
+        return self.address
+
 student1 = Student("Peter Parker", "Landevej 21, 1980, Vesterbro", "32") 
 
 print(student1.name)
 print(student1.age)
 print(student1.address)
+
    
