2ba80acd5d44e1719ec35405ee33d8a909aa682b
MelCop <charliedem20@gmail.com>
2023-04-06 18:15:26 +0200

    databaseøvelse

---
+++ KEA_python/databaseexercise.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+# Opret forbindelse til databasen
+conn = sqlite3.connect('test.db')
+
+# Opret en tabel med kolonnerne 'id', 'navn' og 'alder'
+conn.execute('''CREATE TABLE IF NOT EXISTS personer
+             (id INTEGER PRIMARY KEY AUTOINCREMENT,
+             navn TEXT NOT NULL,
+             alder INTEGER NOT NULL);''')
+
+# Indsæt nogle rækker i tabellen
+conn.execute("INSERT INTO personer (navn, alder) VALUES ('Peter', 27)")
+conn.execute("INSERT INTO personer (navn, alder) VALUES ('Sara', 32)")
+conn.execute("INSERT INTO personer (navn, alder) VALUES ('Mikkel', 19)")
+
+# Gem ændringer og luk forbindelsen til databasen
+conn.commit()
+conn.close()
+
+# Udskriv rækkerne fra tabellen
+conn = sqlite3.connect('test.db')
+cursor = conn.execute("SELECT * FROM personer")
+for row in cursor:
+    print(row)
+conn.close()
