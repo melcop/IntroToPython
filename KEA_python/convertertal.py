9d4ea5a7dda8b5ed01ee194426ffb5a97073e5e1
MelCop <charliedem20@gmail.com>
2023-04-08 09:11:05 +0200

    Convertertal

---
+++ KEA_python/convertertal.py
@@ -0,0 +1,7 @@
+#a = input("Indtast dit binæretal: ")
+#a_i_tal = int(a)
+#b = input("Indtast dit hexdecimal tal: ")
+#b_i_tal = int(b)
+c = 0b10111011
+d = 0xc5f
+print('Binære er %d, hexadecimal er %d' % (c, d))
