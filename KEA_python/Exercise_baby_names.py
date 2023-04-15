ce89afac6bccc668aaf49fece977d2ee7cc28835
MelCop <charliedem20@gmail.com>
2023-04-15 12:06:22 +0200

    test

---
+++ KEA_python/Exercise_baby_names.py
@@ -0,0 +1,9 @@
+baby_names = {
+    'cat': 'kitten',
+    'dog': 'puppy',
+}
+print(baby_names)
+print(list(baby_names.keys()))
+print(list(baby_names.values()))
+print(list(baby_names.items()))
+print(baby_names.popitem())
